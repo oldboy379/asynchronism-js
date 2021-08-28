2a2642b6f50d81b8741a542960696f4b5e23d474
Olman Castro Hernández <olcastro@estudiantec.cr>
2021-08-28 14:00:13 -0600

    [add] Promise

---
+++ src/promise/index.js
@@ -0,0 +1,38 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve('Hey!');
+    } else {
+      reject('Whops!');
+    }
+  });
+};
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve('True');
+      }, 2000)
+    } else {
+      const error = new Error('Whops!');
+      reject(error);
+    }
+  })
+}
+
+somethingWillHappen()
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+somethingWillHappen2()
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(response => {
+    console.log('Array of results', response);
+  })
+  .catch(err => {
+    console.log(err);
+  })
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+let XMLHTTPRequest = require('xmlhttprequest').XMLHttpRequest;
+
+/*
+Fetch con callbacks
+*/
+
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    const xhttp = new XMLHTTPRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = (() => {
+      if (xhttp.readyState === 4) {
+        (xhttp.status === 200)
+        ? resolve(JSON.parse(xhttp.responseText))
+        : reject(new Error('Error', url_api))
+      }
+    });
+    xhttp.send();
+  });
+}
+
+module.exports = fetchData;
